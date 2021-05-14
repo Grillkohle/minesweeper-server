e3804965ce697acca3261b4c6b012144d4b76bcd
Frieder Ulm <frieder@zalando.de>
2021-05-14 13:29:09 +0200

    #9 add mine indicators. also converted cellentity properties to val, ensure that mines and indicators are placed on construction and never changedf

--- src/main/kotlin/minesweeper/repository/entity/CellEntity.kt
+++ src/main/kotlin/minesweeper/repository/entity/CellEntity.kt
@@ -1,5 +1,23 @@
 package minesweeper.repository.entity
 
-data class CellEntity(val horizontalIndex: Int,
+class CellEntity(val horizontalIndex: Int,
                       val verticalIndex: Int,
-                      var isMine : Boolean = false)
+                      val isMine : Boolean = false,
+                      val numberOfAdjacentMines:
+                      Int = 0){
+    companion object {
+        private val neighborCoordinateOffsets = listOf(Pair(-1,-1), Pair(0,-1), Pair(1, -1),
+                                                       Pair(-1, 0),             Pair(1, 0),
+                                                       Pair(-1, 1), Pair(0, 1), Pair(1, 1))
+
+        fun getNeighborCoordinates(coordinates: Pair<Int, Int>, maxCoordinates: Pair<Int, Int>): Set<Pair<Int, Int>> {
+            val (x, y) = coordinates
+            val (maxX, maxY) = maxCoordinates
+
+            return neighborCoordinateOffsets
+                    .map { (xOffset, yOffset) -> x + xOffset to y + yOffset } // generate indices of all possible neighboring fields
+                    .filterNot { (x, y) -> x < 0 || x > maxX || y < 0 || y > maxY} // filter those out which are not on the board
+                    .toSet()
+        }
+    }
+}
--- src/main/kotlin/minesweeper/service/GameFactory.kt
+++ src/main/kotlin/minesweeper/service/GameFactory.kt
@@ -11,37 +11,43 @@ import kotlin.random.Random
 class GameFactory {
     fun createGame(horizontalSize: Int,
                    verticalSize: Int): GameEntity {
-        val cells = List(horizontalSize) { horizontalIndex -> createColumn(horizontalIndex, verticalSize) }
+        val mineIndices = generateMineIndices(horizontalSize, verticalSize)
         val boardEntity = BoardEntity(
                 horizontalSize = horizontalSize,
                 verticalSize = verticalSize,
-                cells = cells,
-                numberOfMines = generateMines(cells, horizontalSize, verticalSize)
+                cells = List(horizontalSize) { horizontalIndex -> createColumn(
+                        horizontalIndex = horizontalIndex, 
+                        boardSize = horizontalSize to verticalSize, 
+                        mineIndices = mineIndices) },
+                numberOfMines = mineIndices.size
         )
 
         return GameEntity(board = boardEntity)
     }
 
-    private fun generateMines(cells: List<List<CellEntity>>, horizontalSize: Int, verticalSize: Int): Int {
-        val numberOfMines = sqrt((horizontalSize * verticalSize).toDouble()).toInt()
-        var placedMinesCounter = 0
-        while (placedMinesCounter < numberOfMines) {
-            val (x, y) = Pair(
-                    Random.nextInt(0, horizontalSize - 1),
-                    Random.nextInt(0, verticalSize - 1))
-            if (!cells[x][y].isMine) {
-                cells[x][y].isMine = true
-                placedMinesCounter++
-            }
-        }
+    private fun generateMineIndices(horizontalSize: Int, verticalSize: Int): Set<Pair<Int, Int>> {
+        val targetNumberOfMines = sqrt((horizontalSize * verticalSize).toDouble()).toInt()
+        val placedMinesIndices: MutableSet<Pair<Int, Int>> = mutableSetOf()
 
-        return numberOfMines
+        while (placedMinesIndices.size < targetNumberOfMines)
+            placedMinesIndices.add(
+                    Random.nextInt(0, horizontalSize - 1) to Random.nextInt(0, verticalSize - 1))
+
+        return placedMinesIndices.toSet()
     }
 
-    private fun createColumn(horizontalIndex: Int, verticalSize: Int): List<CellEntity> {
+    private fun createColumn(horizontalIndex: Int, boardSize: Pair<Int, Int>, mineIndices: Set<Pair<Int, Int>>): List<CellEntity> {
+        val (horizontalSize, verticalSize) = boardSize
         return List(verticalSize) { verticalIndex ->
-            CellEntity(horizontalIndex = horizontalIndex,
-                    verticalIndex = verticalIndex)
+            CellEntity(
+                    horizontalIndex = horizontalIndex,
+                    verticalIndex = verticalIndex,
+                    isMine = mineIndices.contains(horizontalIndex to verticalIndex),
+                    numberOfAdjacentMines = CellEntity.getNeighborCoordinates(
+                            coordinates = horizontalIndex to verticalIndex, 
+                            maxCoordinates = horizontalSize - 1 to verticalSize - 1)
+                            .filter { mineIndices.contains(it) }
+                            .count())
         }
     }
 }
