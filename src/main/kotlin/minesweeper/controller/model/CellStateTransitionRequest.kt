7c8ca5ea072456ab5fd4c3cd6b2b8edb5e729fde
Frieder Ulm <frieder@zalando.de>
2021-05-15 11:52:38 +0200

    #18 make bean validation work for missing properties

--- src/main/kotlin/minesweeper/controller/model/CellStateTransitionRequest.kt
+++ src/main/kotlin/minesweeper/controller/model/CellStateTransitionRequest.kt
@@ -3,7 +3,7 @@ package minesweeper.controller.model
 import javax.validation.constraints.Min
 import javax.validation.constraints.NotNull
 
-data class CellStateTransitionRequest(@get:NotNull @get:Min(0) val horizontalIndex: Int,
-                                      @get:NotNull @get:Min(0) val verticalIndex: Int,
+data class CellStateTransitionRequest(@field:NotNull @field:Min(0) val horizontalIndex: Int?,
+                                      @field:NotNull @field:Min(0) val verticalIndex: Int?,
                                       val state: CellResponseState
 )
