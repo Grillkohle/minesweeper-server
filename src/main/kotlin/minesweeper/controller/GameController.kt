4341d33203487b139954b52fdd7c34bc7dd3a254
Frieder Ulm <frieder@zalando.de>
2021-05-20 17:26:24 +0200

    #5 add exception handler for constraint violation, remove superfluous code

--- src/main/kotlin/minesweeper/controller/GameController.kt
+++ src/main/kotlin/minesweeper/controller/GameController.kt
@@ -2,9 +2,8 @@ package minesweeper.controller
 
 import minesweeper.controller.model.CellStateTransitionRequest
 import minesweeper.controller.model.CellStateTransitionResponse
-import minesweeper.controller.model.Problem
+import minesweeper.controller.model.GameResponse
 import minesweeper.service.GameService
-import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.PatchMapping
@@ -16,61 +15,42 @@ import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.util.UriComponentsBuilder
 import java.util.UUID
 import javax.validation.Valid
+import javax.validation.constraints.Min
 
 @RestController
 @Validated
 class GameController(
-        private val gameService: GameService
+    private val gameService: GameService
 ) {
     @PostMapping("/games")
     fun createGame(
-            @RequestParam("horizontal_size") horizontalSize: Int,
-            @RequestParam("vertical_size") verticalSize: Int,
-            builder: UriComponentsBuilder
-    ): ResponseEntity<*> {
-        validateCreateGameInput(horizontalSize, verticalSize)?.let { return it }
-
+        @RequestParam("horizontal_size") @Min(1) horizontalSize: Int,
+        @RequestParam("vertical_size") @Min(1) verticalSize: Int,
+        builder: UriComponentsBuilder
+    ): ResponseEntity<GameResponse> {
         val gameResponse = gameService.createGame(
-                horizontalSize,
-                verticalSize)
+            horizontalSize,
+            verticalSize
+        )
 
         return ResponseEntity
-                .created(builder.path("/games/${gameResponse.id}")
-                        .build()
-                        .toUri())
-                .body(gameResponse)
+            .created(
+                builder.path("/games/${gameResponse.id}")
+                    .build()
+                    .toUri()
+            )
+            .body(gameResponse)
     }
 
     @PatchMapping("/games/{gameId}/cells")
-    fun changeCellState(@PathVariable gameId: UUID,
-                        @RequestBody @Valid transitionRequest: CellStateTransitionRequest
+    fun changeCellState(
+        @PathVariable gameId: UUID,
+        @RequestBody @Valid transitionRequest: CellStateTransitionRequest
     ): ResponseEntity<CellStateTransitionResponse> {
         val transitionResponse = gameService.updateCellState(gameId, transitionRequest)
 
         return ResponseEntity
-                .ok()
-                .body(transitionResponse)
-    }
-
-    private fun validateCreateGameInput(horizontalSize: Int,
-                                        verticalSize: Int): ResponseEntity<Problem>? {
-        val errorMessages = mutableListOf<String>()
-
-        if (horizontalSize <= 0)
-            errorMessages += "horizontal_size must be greater than 0"
-
-        if (verticalSize <= 0)
-            errorMessages += "vertical_size must be greater than 0"
-
-        return if (errorMessages.isEmpty()) null else buildProblem(errorMessages)
-    }
-
-    private fun buildProblem(errorMessages: List<String>): ResponseEntity<Problem> {
-        return ResponseEntity
-                .badRequest()
-                .body(Problem(title = "Bad request",
-                        status = HttpStatus.BAD_REQUEST.value(),
-                        detail = errorMessages.joinToString(separator = ",")
-                ))
+            .ok()
+            .body(transitionResponse)
     }
 }
